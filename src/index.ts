c5b0be3011fb0ac2123fd5b375d0a7db13de716d
Woo <woo@chequer.io>
2022-03-25 12:50:24 +0900

    Revert "Update grpc to grpc-js"
    This reverts commit a8f3958553690bc40ce1fbcab691c1a32ec26b69.
    
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/experimental/index.ts
    
    Revert "Update grpc to grpc-js"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as grpc from '@grpc/grpc-js';
+/* eslint-disable @typescript-eslint/no-angle-bracket-type-assertion */
+import * as grpc from 'grpc';
 
 import {
   ClientFactoryConstructor,
@@ -14,8 +15,7 @@ import {
 export { grpc, ClientFactoryConstructor };
 
 export interface GenericServerBuilder<T> {
-  start(address: string, credentials?: grpc.ServerCredentials): Promise<void>;
-
+  start(address: string, credentials?: grpc.ServerCredentials): void;
   forceShutdown(): void;
 }
 
@@ -25,23 +25,15 @@ export function serverBuilder<T>(
   server = new grpc.Server(),
   includeDirs?: string[],
 ): T & GenericServerBuilder<T> {
-  const builder: DynamicMethods = {
-    async start(address, credentials) {
-      return new Promise((resolve, reject) => {
-        server.bindAsync(address, credentials || grpc.ServerCredentials.createInsecure(), error => {
-          if (error) {
-            reject(error);
-            return;
-          }
-          server.start();
-          resolve();
-        });
-      });
+  const builder: DynamicMethods = <GenericServerBuilder<T>>{
+    start(address, credentials) {
+      server.bind(address, credentials || grpc.ServerCredentials.createInsecure());
+      server.start();
     },
     forceShutdown() {
       server.forceShutdown();
     },
-  } as GenericServerBuilder<T>;
+  };
 
   const pkg = lookupPackage(grpcLoad(protoPath, includeDirs), packageName);
   for (const name of getServiceNames(pkg)) {
@@ -58,7 +50,6 @@ export function serverBuilder<T>(
 export function clientFactory<T>(protoPath: string, packageName: string, includeDirs?: string[]) {
   class Constructor {
     readonly __args: [string, grpc.ChannelCredentials, any | undefined];
-
     constructor(address: string, credentials?: grpc.ChannelCredentials, options: any = undefined) {
       this.__args = [address, credentials || grpc.credentials.createInsecure(), options];
     }
--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ import {
   handleUnaryCall,
   loadPackageDefinition,
   ServiceDefinition,
-} from '@grpc/grpc-js';
+} from 'grpc';
 import { Observable } from 'rxjs';
 
 export type ClientFactoryConstructor<T> = new (
